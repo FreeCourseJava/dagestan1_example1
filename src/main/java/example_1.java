415c921373b457c112b109284b98183cba205a60
dagestan1 <dag0071@mail.ru>
2022-06-14 22:52:43 +0300

    lesson_1

---
+++ src/main/java/example_1.java
@@ -0,0 +1,15 @@
+public class example_1 {
+    public static void main(String[] args) {
+        int balance = 100000, increase = 500000;
+        int TotalScore = NewBalance(balance, increase);
+        System.out.println(TotalScore);
+    }
+
+    public static int NewBalance(int balance, int increase) {
+        int sum;
+        sum = balance + increase;
+        return sum;
+    }
+}
+
+// Переполнение инта
