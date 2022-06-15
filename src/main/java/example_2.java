901716703227721f2617ba1239720e2fd4ffc827
dagestan1 <dag0071@mail.ru>
2022-06-15 17:01:08 +0300

    3 задания

---
+++ src/main/java/example_2.java
@@ -0,0 +1,10 @@
+public class example_2 {
+    public static void main(String[] args) {
+        char sym = 'a';
+        int number = 22;
+        sym  += number;
+        System.out.println(sym);
+        sym -= number;
+        System.out.println(sym);
+    }
+}
---
+++ src/main/java/example_3.java
@@ -0,0 +1,9 @@
+public class example_3 {
+    public static void main(String[] args) {
+        number(75);
+    }
+
+    public static void number(int sheep) {
+        System.out.println("Результат равен: " + sheep + " овец");
+    }
+}
