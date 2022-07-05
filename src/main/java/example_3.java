540cd903a92120e63baa61e12c904de9f4813a00
dagestan1 <107101387+dagestan1@users.noreply.github.com>
2022-07-05 22:07:32 +0300

    Delete example_3.java
    Signed-off-by: dagestan1 <107101387+dagestan1@users.noreply.github.com>

--- src/main/java/example_3.java
+++
@@ -1,9 +0,0 @@
-public class example_3 {
-    public static void main(String[] args) {
-        number(75);
-    }
-
-    public static void number(int sheep) {
-        System.out.println("Результат равен: " + sheep + " овец");
-    }
-}
