2747b1c67c5fc3c5a77848d8841e8f5efb8bb98b
dagestan1 <dag0071@mail.ru>
2022-07-06 18:10:45 +0300

    исправление по комментариям второго и третьего задания

--- src/main/java/week3/week3_second.java
+++ src/main/java/week3/week3_second.java
@@ -6,15 +6,20 @@ public class week3_second {
     public static void main(String[] args) {
         int[] date = {1920, 1438, 1240, 1990, 1796, 1680, 1438, +
                 1552, 1796, 2015, 1708, 1236, 1725, 1723, 1759, 1805, 1918, 1952, 1937, 2012};
-        for (int i = 0; i < date.length-1; i++) {
-            for (int j = 0; j < date.length-1; j++) {
-                if(date[j] > date[j+1]) {
-                    int plus = date[j+1];
-                    date[j+1] = date[j];
-                    date[j] = plus;
+        int[] dateSorted = bubbleSort(date);
+        System.out.println(Arrays.toString(dateSorted));
+    }
+
+    private static int[] bubbleSort(int[] date) {
+        for (int i = 0; i < date.length - 1; i++) {
+            for (int j = 0; j < date.length - 1; j++) {
+                if (date[j] > date[j + 1]) {
+                    int change = date[j + 1];
+                    date[j + 1] = date[j];
+                    date[j] = change;
                 }
             }
         }
-        System.out.println(Arrays.toString(date));
+        return date;
     }
 }
